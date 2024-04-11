20f8a9d2bc29ac701a12a2727032c68e4bee3b7f
Abhinav12-alt <153630910+Abhinav12-alt@users.noreply.github.com>
2024-04-11 23:49:01 +0530

    Update Contact.js

--- client/src/Contact.js
+++ client/src/Contact.js
@@ -7,21 +7,9 @@ function Contact() {
       <div className="contact-section">
         <h2>Contact Us</h2>
         <p>If you have any questions or inquiries, feel free to get in touch with us. We'd love to hear from you!</p>
-        <form className='contactform'>
-          <div className="form-group">
-            <label htmlFor="name">Name:</label>
-            <input type="text" id="name" name="name" />
-          </div>
-          <div className="form-group">
-            <label htmlFor="email">Email:</label>
-            <input type="email" id="email" name="email" />
-          </div>
-          <div className="form-group">
-            <label htmlFor="message">Message:</label>
-            <textarea id="message" name="message" rows="4"></textarea>
-          </div>
-          <button type="submit">Submit</button>
-        </form>
+        <p><i class="fa-solid fa-phone fa-shake"></i> 1234567891</p>
+        <p><i class="fa-solid fa-envelope fa-bounce"></i> @gmail.com</p>
+     
       </div>
     </div>
     </div>
