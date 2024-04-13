212829e9001fbffbe28dc178ccbb146cb20117a2
Abhinav12-alt <153630910+Abhinav12-alt@users.noreply.github.com>
2024-04-13 23:03:04 +0530

    Update Home.js

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,43 +1,86 @@
-// import React,{useEffect,useState} from 'react'
-// import axios
-//  from 'axios'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import './Home.css'
 
 function Home() {
-  // const[auth,setAuth]=useState(false)
-  // const[name,SetName]=useState('')
-  // const[message,SetMessage]=useState('')
-  // useEffect(()=>{
-  //   axios.get('http://localhost:3001/')
-  //   .then(res=>{
-  //     if(res.data.status ==="Success"){
-  //     setAuth(true);
-  //     SetName(res.data.name);
-  //     }else{
-  //       SetMessage(res.data.Message);
-  //     }
-  //     })
-  //   },[])
-  
+  // modal
+ 
+  const [selectedRide, setSelectedRide] = useState(null);
+  const [rides, setRides] = useState([]);
+ 
+  const getRides = () => {
+    axios.post("http://localhost:3001/getride")
+      .then((res) => {
+        console.log(res.data);
+        setRides(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
+  useEffect(() => {
+    getRides();
+  }, []);
+  const openModal = (ride) => {
+    setSelectedRide(ride);
+  }
+
+  const closeModal = () => {
+    setSelectedRide(null);
+  }
   return (
-    
     <div>
-      
-      {/* {
-        auth ?
-        <div>
-          <h3>Your are Authorized{name}</h3>
-          <button className='btn btn-danger'>Logout</button>
-        </div>:
-        <div>
-          <h3>{message}</h3>
-          <h3>Login now</h3>
+      <img id="bannerimage" src="https://media.istockphoto.com/id/639581692/photo/bestfriends-driving-dodgems.jpg?s=612x612&w=0&k=20&c=cZPiiP-Z4fczhERb0u6yDCbPAXccFGKsPyYf5_rAfcY="/>
+    
+      <div className='container'>
+        <div className='row justify-content-center'>
+          {rides.map((ele, index) => (
+            <div key={index} className='col-sm-12 col-md-6 col-lg-4 mb-4 ml-5'>
+              <div className='card h-70  text-center p-4 card shadow'>
+                <img
+                  className='card-img-top img-fluid'
+                  src={`http://localhost:3001/${ele.image}`}
+                  alt='Ride'
+                  style={{ height: "280px", objectFit: 'cover' }}
+                />
+                <div className='card-body'>
+                  <h5 className='card-title mb-4' id="heading">Ride name</h5>
+                  <h6 className='card-title mb-4'>{ele.ridename}</h6>
+                  <Button id="issuedel" variant='danger' onClick={() => openModal(ele)}>View Details</Button>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
-} */}
-     <img id ="imgg"src="https://images.unsplash.com/photo-1432821596592-e2c18b78144f?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGhvbWUlMjBwYWdlfGVufDB8fDB8fHww"></img>
+      </div>
+      {selectedRide && (
+        <Modal show={true} onHide={closeModal}>
+          <Modal.Header closeButton>
+            <Modal.Title>Ride Details</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <img
+              src={`http://localhost:3001/${selectedRide.image}`}
+              alt='Ride'
+              style={{ width: '100%', height: 'auto' }}
+            />
+            <h5>Ride name</h5><br></br> <p>{selectedRide.ridename}</p>
+            <h5>Description</h5> <br></br><p>{selectedRide.description}</p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={closeModal}>
+              Close
+            </Button>
+           
+          </Modal.Footer>
+        </Modal>
+      )}
     </div>
-      
   )
 }
 
 export default Home
+
